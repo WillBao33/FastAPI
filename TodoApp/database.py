ab29f8193c2b5885c027241433c5e95550c0c71d
William Bao <williambao@Williams-MacBook-Pro.local>
2025-08-19 15:25:15 -0400

    Added PostgreSQL and MySQL database, and initialized alembic

--- TodoApp/database.py
+++ TodoApp/database.py
@@ -2,9 +2,11 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-SQLALCHEMY_DATABASE_URL = 'sqlite:///./todosapp.db'
-
-engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={'check_same_thread': False})
+# SQLALCHEMY_DATABASE_URL = 'sqlite:///./todosapp.db' # if use sqlite
+# SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:test1234!@localhost/TodoApplicationDatabase' # if use Postgresql
+SQLALCHEMY_DATABASE_URL = 'mysql+pymysql://root:test1234!@127.0.0.1:3306/TodoApplicationDatabase'  # if use mysql
+# engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={'check_same_thread': False}) for sqlite only
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
--- TodoApp/models.py
+++ TodoApp/models.py
@@ -12,6 +12,7 @@ class Users(Base):
     hashed_password = Column(String) # encrypting a password that cannot be unencrypted
     is_active = Column(Boolean, default=True)
     role = Column(String)
+    phone_number = Column(String)
 
 class Todos(Base):
     __tablename__ = "todos" # name the table in the database
