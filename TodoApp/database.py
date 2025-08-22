d5891f25d03f20ae4e29e20060e89059fb22c967
William Bao <williambao@Williams-MacBook-Pro.local>
2025-08-22 12:42:36 -0400

    Added draft FastAPI test units

--- TodoApp/database.py
+++ TodoApp/database.py
@@ -2,9 +2,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-# SQLALCHEMY_DATABASE_URL = 'sqlite:///./todosapp.db' # if use sqlite
+SQLALCHEMY_DATABASE_URL = 'sqlite:///./todosapp.db' # if use sqlite
 # SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:test1234!@localhost/TodoApplicationDatabase' # if use Postgresql
-SQLALCHEMY_DATABASE_URL = 'mysql+pymysql://root:test1234!@127.0.0.1:3306/TodoApplicationDatabase'  # if use mysql
+# SQLALCHEMY_DATABASE_URL = 'mysql+pymysql://root:test1234!@127.0.0.1:3306/TodoApplicationDatabase'  # if use mysql
 # engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={'check_same_thread': False}) for sqlite only
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
